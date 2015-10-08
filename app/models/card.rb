e9bbff1d5b89d7cb9448f5ff5ba8670fc3b225f3
Max <maximpopov13@gmail.com>
2015-10-08 22:19:19 +0300

    update cards controller

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,12 @@
+class Compare_Validator < ActiveModel::Validator
+  def validate(record)
+    if record.translated_text.downcase  == record.original_text.downcase
+      record.errors[:base] << "Translated and Original texts are identic"
+    end
+  end
+end
+
+
 class Card < ActiveRecord::Base
+	validates_with Compare_Validator
 end
