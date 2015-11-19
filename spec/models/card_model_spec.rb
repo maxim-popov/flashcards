d31e48a7e68616e1d0d34b51e0af67070eb1e71b
Max <maximpopov13@gmail.com>
2015-11-19 19:12:36 +0300

    change in card_model_test

--- spec/models/card_model_spec.rb
+++ spec/models/card_model_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 
 RSpec.describe Card, type: :model do
   context "#check_translation method" do
-    let(:card) {card = Card.new(original_text: "USA", translated_text: "Америка") }
+    let(:card) {create(:card, original_text: "USA", translated_text: "Америка") }
     
     it "should be corret" do
       expect(card.check_translation("АмЕрика")).to eq(true)
